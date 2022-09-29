a6fefbf7ede6e191d7ebe61d764511a0627ec0a7
aditya ray <aditya@pop-os.localdomain>
2022-09-29 17:31:15 +0530

    code cleaning

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-var playground = document.getElementById("playground")
+var playground = document.getElementById("playground");
+var flips = 0, memory = [], matched = [], erase = true, moves = 0;
+var characters = ["I", "T", "C", "H", '"', '!', "B", "A"]
+var available = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+var time = 0, watch;
+
 for (var n = 0; n < 16; n++) {
-  const box = document.createElement("button");
+  const box = document.createElement("button"),
+    boxInner = document.createElement("div"),
+    boxFront = document.createElement("div"),
+    boxBack = document.createElement("div");
   box.classList.add("box")
   box.setAttribute("onclick", "flip(this)")
   playground.appendChild(box);
-
-  const boxInner = document.createElement("div");
   boxInner.classList.add("box-inner")
   box.appendChild(boxInner);
-
-  const boxFront = document.createElement("div");
   boxFront.classList.add("box-front")
   boxInner.appendChild(boxFront);
-
-  const boxBack = document.createElement("div");
   boxBack.classList.add("box-back")
   boxInner.appendChild(boxBack);
 }
-var flips = 0;
-var memory = []
-var matched = []
-var erase = true;
-var moves = 0;
+
 function flip(e) {
   if (time === 0) {
     startTimer();
@@ -36,13 +34,12 @@ function flip(e) {
       matched.push(memory[0])
       matched.push(memory[1])
       erase = false;
-if(matched.length===16){
-  alert("you win!")
-  clearTimeout(watch)
-}
+      if (matched.length === 16) {
+        alert("you win!")
+        clearTimeout(watch)
+      }
     }
   } else if (flips > 2) {
-
     moves += 1
     var move = moves < 10 ? "0" + moves : moves;
     document.getElementById("moves").innerHTML = "moves: " + move;
@@ -55,29 +52,24 @@ if(matched.length===16){
       memory = memory.filter(item => (item !== memory[0]) && (item !== memory[1]))
       flips = 1;
       erase = true;
-
     }
-
   }
 }
 
-var characters = ["I", "T", "C", "H", '"', '!', "B", "A"]
-
-var available = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
 for (const char of characters) {
-  let index = available[Math.floor(Math.random() * available.length)]
-  available = available.filter(item => item !== index)
-  document.getElementsByClassName("box-back")[index].innerHTML = char;
-  let index2 = available[Math.floor(Math.random() * available.length)]
-  available = available.filter(item => item !== index2)
-  document.getElementsByClassName("box-back")[index2].innerHTML = char;
+  let index = available[Math.floor(Math.random() * available.length)];
+  let boxBack = document.getElementsByClassName("box-back");
+  available = available.filter(item => item !== index);
+  boxBack[index].innerHTML = char;
+  let index2 = available[Math.floor(Math.random() * available.length)];
+  available = available.filter(item => item !== index2);
+  boxBack[index2].innerHTML = char;
 }
-var time = 0;
-var watch;
+
 function startTimer() {
-  watch =setInterval(timer, 1000)
+  watch = setInterval(timer, 1000)
 }
+
 function msToTime(duration) {
   var milliseconds = Math.floor((duration % 1000) / 100),
     seconds = Math.floor((duration / 1000) % 60),
@@ -90,6 +82,7 @@ function msToTime(duration) {
 
   return hours + ":" + minutes + ":" + seconds;
 }
+
 function timer() {
   time += 1000;
   document.getElementById("timer").innerHTML = msToTime(time)
